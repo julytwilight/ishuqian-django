f36729564386b91a23c6223e04d7bba44b8c919f
2forVendetta <julytwilight@msn.com>
2013-08-12 21:56:55 +0800

    d

--- ishuqian/urls.py
+++ ishuqian/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -54,4 +55,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
++ staticfiles_urlpatterns()
+
+if settings.DEBUG is False:   #if DEBUG is True it will be served automatically
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
